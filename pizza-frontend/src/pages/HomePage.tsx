42d518bc564907b6a475b224eb283b8a5be91f40
PhamVanTea <23633471.tra@student.iuh.edu.vn>
2025-07-22 09:02:51 +0700

    feat: get api

--- pizza-frontend/src/pages/HomePage.tsx
+++ pizza-frontend/src/pages/HomePage.tsx
@@ -1,26 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CardPizza from "../sections/CardPizza";
 import type { Pizza } from "../models/pizza.model";
+import CountPizza from "../sections/CountPizza";
 
 const HomePage = () => {
-  const [pizzas, setPizzas] = useState<Pizza[]>([
-    { id: 1, title: "Pizza thit bam", description: "Thit bam, sot ca chua" },
-    { id: 2, title: "Pizza hai san", description: "Hai san, sot ca chua" },
-    { id: 3, title: "Pizza pho mai", description: "Pho mai, sot ca chua" },
-  ]);
+  const [pizzas, setPizzas] = useState<Pizza[]>([]);
+  const [count, setCount] = useState(0);
+  const [isCount, setIsCount] = useState(false);
+
+  useEffect(() => {
+    fetch("http://localhost:4001/products")
+      .then((res) => res.json())
+      .then(({ data }) => setPizzas(data));
+  }, []);
 
   return (
     <>
-      <div className="px-60 py-20">
-        <div className="grid grid-cols-3 gap-6 mt-10">
-          {pizzas.map((item) => (
+      {console.log("render template")}
+      <div className="px-4 md:px-10 lg:px-20 xl:px-60 py-10">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
+          {pizzas.map((item, index) => (
             <CardPizza
+              key={index}
               id={item.id}
-              title={item.title}
+              productName={item.productName}
               description={item.description}
             />
           ))}
         </div>
+        <button
+          onClick={() => setIsCount(true)}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1.5 px-3 rounded cursor-pointer text-sm"
+        >
+          Open Count
+        </button>
+        <button
+          onClick={() => setIsCount(false)}
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-1.5 px-3 rounded cursor-pointer text-sm"
+        >
+          Close count
+        </button>
+
+        {isCount && (
+          <CountPizza count={count} setCount={(count) => setCount(count)} />
+        )}
+        <br />
+        {count}
       </div>
     </>
   );
--- pizza-frontend/src/sections/CardPizza.tsx
+++ pizza-frontend/src/sections/CardPizza.tsx
@@ -3,15 +3,17 @@ import type { Pizza } from "../models/pizza.model";
 
 type Props = Pizza;
 
-const CardPizza = ({ title, description }: Props) => {
+const CardPizza = ({ productName, description }: Props) => {
   return (
     <>
-      <div className="overflow-y-auto">
+      <div className="overflow-y-auto cursor-pointer">
         <div className="w-100 rounded overflow-hidden shadow-lg bg-gray-100">
           <img src={LogoPizza} alt="pizza-image" />
           <div className="bg-[#14274E]">
             <div className="px-4 py-3">
-              <div className="font-bold text-white text-lg mb-2">{title}</div>
+              <div className="font-bold text-white text-lg mb-2">
+                {productName}
+              </div>
               <p className="text-white text-base">{description}</p>
             </div>
             <div className="px-4 pt-3 pb-2">
